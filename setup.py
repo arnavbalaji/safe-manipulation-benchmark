4aa15b866740c3224a6f237f091caa8fc5378479
Arnav Balaji <arnavbalaji21@gmail.com>
2025-07-31 08:57:00 -0500

    setup changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safety_benchmark",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Safe Manipulation Benchmark for Robot Learning",
+    author="",  # Add author info if desired
+    python_requires=">=3.7",
+) 
